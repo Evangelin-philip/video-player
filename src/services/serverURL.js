43bdf55da921476c9ee533c9a2fb10a53698e419
Evangelin-philip <evangelingeorge13@gmail.com>
2023-10-13 13:02:01 +0530

    server url updated

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,5 @@
- export const server_url = "http://localhost:4000"
+//  export const server_url = "http://localhost:4000"
+export const server_url = "https://videoplayerserver.onrender.com"
 // don't need slash at the end
+
+
